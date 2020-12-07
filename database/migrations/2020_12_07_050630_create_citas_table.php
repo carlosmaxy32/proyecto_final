e12005950cd1ecbddb9bf1fb70007b04cf3742b2
carlosmaxy32 <carlos.moreno6564@alumnos.udg.mx>
2020-12-06 23:21:47 -0600

    Creacion modelo, controlador y migracion de la tabla citas

---
+++ database/migrations/2020_12_07_050630_create_citas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contacto_id')->references('id')->on('dentista_paciente')->onDelete('cascade');
+            $table->date('fecha');
+            $table->time('hora');
+            $table->foreignId('servicio_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+}
