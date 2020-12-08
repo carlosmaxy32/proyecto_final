8f67ff09e98fccdbe8aba5af3159d89c6505489e
carlosmaxy32 <carlos.moreno6564@alumnos.udg.mx>
2020-12-08 08:39:17 -0600

    Algunos cambios

--- app/Models/Cita.php
+++ app/Models/Cita.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Cita extends Model
 {
     use HasFactory;
-   // use SoftDeletes;
+    use SoftDeletes;
 
     protected $fillable = [
         'contacto_id',
